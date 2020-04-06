06d1c962617b32249155ec6dc286d4b93c528d69
jordanliu <jordanxavierliu@gmail.com>
2020-04-06 00:31:51 -0500

    Added homepage

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./Home.scss"
+
+const Home = () => {
+  return (
+    <div className="home-wrapper">
+      <h1>Escape the Hacker</h1>
+      <div className="button">Start</div>
+    </div>
+  )
+}
+
+export default Home
