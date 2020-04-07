8a29be6a0e39ec090de8c9d570a165b14a127e23
jordanliu <jordanxavierliu@gmail.com>
2020-04-07 00:23:18 -0500

    Added timer

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -2,11 +2,21 @@ import React from "react"
 import "./Timer.scss"
 import { Switch, Route } from "react-router-dom"
 import { Question, Level1 } from "../../components"
+import { useTimer } from "react-timer-hook"
 const Timer = () => {
+  const time = new Date()
+  time.setSeconds(time.getSeconds() + 600) // 10 minutes timer
+  let expiryTimestamp = time
+  const { seconds, minutes } = useTimer({
+    expiryTimestamp,
+    onExpire: () => alert("You lose!"),
+  })
   return (
     <div>
       <div className="timer-wrapper">
-        <h1 className="timer">10:00</h1>
+        <h1 className="timer">
+          {minutes}:{seconds}
+        </h1>
       </div>
       <Switch>
         <Route exact path="/start" component={Question} />
