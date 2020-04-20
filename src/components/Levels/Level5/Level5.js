7d628c3946dee327687565966b9e9f05e0fdb8b3
jordanliu <jordanxavierliu@gmail.com>
2020-04-20 12:58:56 -0500

    Added lvl5

--- src/components/Levels/Level5/Level5.js
+++ src/components/Levels/Level5/Level5.js
@@ -1,22 +1,15 @@
-import React, { useRef } from "react"
-//import { useHistory } from "react-router-dom"
+import React from "react"
+import { Link } from "react-router-dom"
 import qr from "./qr.png"
 import "./Level5.scss"
 
 const Level5 = React.memo(() => {
-  // const history = useHistory()
-  // const [text, setText] = useState("")
-  const inputRef = useRef()
-  // useEffect(() => {
-  //   if (text === "T54321") {
-  //     history.push("/level/6")
-  //   }
-  // }, [history, text])
-
   return (
     <div className="level5-wrapper">
+      <span className="hidden">
+        <Link to="/level/6">Click Here</Link>
+      </span>
       <img src={qr} alt="qr code" />
-      <input type="text" ref={inputRef} />
     </div>
   )
 })
